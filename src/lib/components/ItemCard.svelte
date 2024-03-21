8e1bbde9b5f64a480d67640dc193ccd1622d9e95
DanielSSW <daniel.rojas@digitashealth.com>
2024-03-21 13:12:08 -0600

    Initial commit

---
+++ src/lib/components/ItemCard.svelte
@@ -0,0 +1,12 @@
+<div class="card w-56 bg-base-100 shadow-xl">
+  <figure><img src="/chat.webp" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">AI CHATBOT</h2>
+    <p class="py-4">The first Vercel AI SDK Project</p>
+    <div class="card-actions">
+      <a href="/" class="w-full">
+      <button class="btn bg-[#e7e7e7] text-neutral w-full">GO!</button>
+    </a>
+    </div>
+  </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import ItemCard from "../lib/components/ItemCard.svelte";
+</script>
+
+<div class="relative grid grid-cols-1 md:grid-cols-2 gap-16 lg:w-full py-24 justify-items-center mx-auto">
+<ItemCard/>
+</div>
