a854babfb413c17aaa60b5639b01b8435def5ae0
DanielSSW <daniel.rojas@digitashealth.com>
2024-03-21 14:44:20 -0600

    Update ItemCard.svelte

--- src/lib/components/ItemCard.svelte
+++ src/lib/components/ItemCard.svelte
@@ -2,7 +2,7 @@
   <figure><img src="/chat.webp" alt="Shoes" /></figure>
   <div class="card-body">
     <h2 class="card-title">AI CHATBOT</h2>
-    <p class="py-4">The first Vercel AI SDK Project</p>
+    <p class="py-4">The first Vercel AI SDK Project GIVE ME MONEY!</p>
     <div class="card-actions">
       <a href="https://ai-chat-eosin-iota.vercel.app/" class="w-full">
       <button class="btn bg-[#e7e7e7] text-neutral w-full">GO!</button>
